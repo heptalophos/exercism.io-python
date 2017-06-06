34fa2a2c24ff1d7a177201c3f9473293cf1232d0
Chris <cnp.a.002@gmail.com>
2017-06-07 01:20:00 +0300

    pangram

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,8 @@
-def is_pangram():
-    pass
+import re
+
+dict = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def normal(text): return re.sub(r'\W+|_|\d+', '', text)
+
+def is_pangram(text):
+    return set(normal(text).lower()) == set(dict.lower())
