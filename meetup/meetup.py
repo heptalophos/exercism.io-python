099434b4f4d12598290ef18ce11d6361a1049291
Chris <cnp.a.002@gmail.com>
2017-08-13 19:30:47 +0300

    meetup

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -7,22 +7,23 @@ def last(d):
     return d.month != (d + datetime.timedelta(days=7)).month
 
 def nth(n):
-    inweek = lambda d: (d.day - 1) // 7 == n - 1
+    inweek = lambda d: (d.day - 1) // 7 == n 
     return inweek
 
 def teenth(d):
     return (d.day > 12 and d.day < 20)
 
-WHICH = {'teenth': teenth, 'last': last, '1st': nth(1), '2nd': nth(2), '3rd': nth(3), '4th': nth(4), '5th': nth(5)}
+WHICH = {'teenth': teenth, 'last': last, '1st': nth(0), '2nd': nth(1), '3rd': nth(2), '4th': nth(3), '5th': nth(4)}
 
 def meetup_day(year, month, weekday, which):
 
     day = datetime.date(year, month, 1)
-    
+
     while day.month == month:
         if day.weekday() == WEEKDAY[weekday] and WHICH[which](day):
             return day
         day += datetime.timedelta(days=1)
+    
     raise Exception("Not a valid meetup day")
 
 
