7039df52be199aaf545aee497f4495316c19bc2c
Chris NP <zerodeltahedge@gmail.com>
2018-04-20 01:48:17 +0300

    added rotational-cipher

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,10 @@
-def rotate():
-    pass
+def rotate(text, key):
+    return ''.join(rotation(c, key) for c in text)
+
+
+def rotation(c, k):
+    if c.islower():
+        return chr((ord(c) - ord('a') + k) % 26 + ord('a'))
+    elif c.isupper():
+        return chr((ord(c) - ord('A') + k) % 26 + ord('A'))
+    return c
