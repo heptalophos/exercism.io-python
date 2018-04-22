d8625bc9503950391b26cf3c4de2c7e39d31f458
Chris NP <zerodeltahedge@gmail.com>
2018-04-22 23:42:06 +0300

    added allergies

---
+++ allergies/allergies.py
@@ -0,0 +1,28 @@
+class Allergies(object):
+
+    allergies = [ 
+        "eggs",
+        "peanuts",
+        "shellfish",
+        "strawberries",
+        "tomatoes",
+        "chocolate",
+        "pollen",
+        "cats"
+        ]   
+
+    def __init__(self, score):
+        self.score = score
+
+    def is_allergic_to(self, item):
+        if self.score & 1 << self.allergies.index(item) >= 1:
+            return True
+        elif self.score & 1 << self.allergies.index(item) == 0:
+            return False
+        else:
+            return self.score & 1 << self.allergies.index(item)
+
+    @property
+    def lst(self):
+        return [allergy for allergy in self.allergies
+                if self.is_allergic_to(allergy)]
