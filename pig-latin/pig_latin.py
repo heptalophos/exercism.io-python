7b947c91aba02ad723414f4b7b80d8685ad87569
Zero Delta <zerodeltahedge@gmail.com>
2018-09-19 05:01:36 +0300

    pig-latin refactor, all tests passing

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,25 +1,18 @@
-import re
-from string import ascii_lowercase
-
-def pigword (text):
-    # head = re.search('{0}|{1}|{2}'
-    #          .format('[aeio]', '((?<!q)u)', '(^(x|y)(?=[{0}]))')
-    #          .format(''.join(set(ascii_lowercase) - set('aeiouy'))), text)
-    # if head:
-    #     text = text[head.start(0):] + text[:head.start(0)]
-    # for form in (r'(.*?[^q])(u.*)', r'(^y[^aeiou].*)()', r'(.*?)([aeoi].*)'):
-    formv = r'^(a|e|i|o|u|yt|xr)(\\w+)$'
-    formc = r'^(y|ch|qu|thr|th|sch|yt|rh|\\wqu|\\w)(\\w+)$'
-    if (re.search(formv, text) != None):
-        pigl = re.sub(formv, r'\1\2ay', text)
-    elif (re.search(formc, text) != None):
-        pigl = re.sub(formc, r'\2\1ay', text)
-    else: 
-        pigl = text + 'ay'
-    return pigl
+def pig_latinize(word):
+    if word.startswith (('a', 'e', 'i', 'o', 'u')):
+	    return word + 'ay'
+    if word.startswith (('thr', 'sch', 'squ')):
+	    return word[3:] + word[:3] + 'ay'
+    if word.startswith (('ch', 'qu', 'th', 'rh')):
+	    return word[2:] + word[:2] + 'ay'
+    if word.startswith (('ye', 'xe')):
+	    return word[1:] + word[:1] + 'ay'
+    if word.startswith (('yt', 'xr')):
+	    return word + 'ay'
+    return word[1:] + word[:1] + 'ay'
         
 
-def translate(text):
-    return ' '.join([pigword(w) for w in text.lower().split()])
+def translate(sentence):
+    return ' '.join([pig_latinize(word) for word in sentence.lower().split()])
 
             
