7b3281fd1df68e45bd6627f99e72a3679d87a489
Zero Delta <zerodeltahedge@gmail.com>
2018-09-20 00:37:40 +0300

    scrabble-score

---
+++ scrabble-score/scrabble_score.py
@@ -0,0 +1,19 @@
+SCORES = {
+    1: 'aeioulnrst',
+    2: 'dg',
+    3: 'bcmp',
+    4: 'fhvwy',
+    5: 'k',
+    8: 'jx',
+    10: 'qz',
+    }
+
+SCORES = {letter.upper(): score
+         for score in SCORES
+         for letter in SCORES[score]}
+
+def score(word):
+    if word.isalpha():
+        return sum(SCORES[c] for c in word.upper())
+    return 0
+    
