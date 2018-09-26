246a1fc9b34144f32cc51f51a891a05cd35fd3ea
Zero Delta <zerodeltahedge@gmail.com>
2018-09-26 17:10:35 +0300

    saddle-points refactor

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -3,9 +3,10 @@ def saddle_points(matrix):
         return set()
     if any(len(row) != len(matrix[0]) for row in matrix):
             raise ValueError("irregular matrix")
+    columns = list(zip(*matrix))
     return {(rx, cx) for rx, row in enumerate(matrix)
                      for cx, col in enumerate(row)
-                     if min(list(zip(*matrix))[cx]) == max(row)}
+                     if min(columns[cx]) == max(row)}
 
     # def transpose(m):
     #     return [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
