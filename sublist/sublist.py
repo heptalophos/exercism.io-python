550d9dcb39b3bb51c60aa7778794120cc09924cd
Zero Delta <zerodeltahedge@gmail.com>
2018-10-26 23:00:49 +0300

    Update sublist/sublist.py

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -1,3 +1,11 @@
+SUBLIST, SUPERLIST, EQUAL, UNEQUAL = 3, 2, 1, 0
+
 def check_lists(first_list, second_list):
-    
-    pass
+    if first_list == second_list:
+        return EQUAL * int(xs == ys)
+    first_list, second_list = ''.join(map(str, first_list)), ''.join(map(str, second_list))
+    if second_list.find(first_list) != -1:
+        return SUBLIST
+    if first_list.find(second_list) != -1:
+        return SUPERLIST
+    return UNEQUAL
