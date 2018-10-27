56917603d5f733b1eda5b76024a45e7b39fe6f0b
Zero Delta <zerodeltahedge@gmail.com>
2018-10-27 23:38:58 +0300

    Update sublist/sublist.py

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -1,11 +1,15 @@
 SUBLIST, SUPERLIST, EQUAL, UNEQUAL = 3, 2, 1, 0
 
 def check_lists(first_list, second_list):
-    if first_list == second_list:
-        return EQUAL * int(xs == ys)
-    first_list, second_list = ''.join(map(str, first_list)), ''.join(map(str, second_list))
-    if second_list.find(first_list) != -1:
+    if first_list == [] and second_list:
         return SUBLIST
-    if first_list.find(second_list) != -1:
+    if second_list == [] and first_list:
+        return SUPERLIST 
+    if first_list == second_list:
+        return EQUAL
+    first_list, second_list = ''.join(map(str, first_list)), ''.join(map(str, second_list))
+    if second_list.find(first_list) > -1:
+        return SUBLIST
+    if first_list.find(second_list) > -1:
         return SUPERLIST
     return UNEQUAL
