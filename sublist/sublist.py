927696b892d3595bcd84c3ef3f667bf10e9a4d53
Zero Delta <zerodeltahedge@gmail.com>
2018-11-20 23:31:32 +0200

    refactor sublist

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -1,15 +1,16 @@
 SUBLIST, SUPERLIST, EQUAL, UNEQUAL = 3, 2, 1, 0
 
-def check_lists(first_list, second_list):
-    if first_list == [] and second_list:
+
+def check_lists(first, second):
+    if first == [] and second:
         return SUBLIST
-    if second_list == [] and first_list:
+    if second == [] and first:
         return SUPERLIST 
-    if first_list == second_list:
+    if first == second:
         return EQUAL
-    first_list, second_list = ''.join(map(str, first_list)), ''.join(map(str, second_list))
-    if second_list.find(first_list) > -1:
+    first, second = ''.join(map(str, first)), ''.join(map(str, second))
+    if second.find(first) > -1:
         return SUBLIST
-    if first_list.find(second_list) > -1:
+    if first.find(second) > -1:
         return SUPERLIST
     return UNEQUAL
