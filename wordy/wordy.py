3440aa64931c71969823bc2c9228f1d185cc7213
Zero Delta <zerodeltahedge@gmail.com>
2019-06-28 21:36:32 +0300

    Create wordy.py

---
+++ wordy/wordy.py
@@ -0,0 +1,9 @@
+iadd = lambda x, y : x + y
+isub = lambda x, y : x - y
+imul = lambda x, y : x * y
+idiv = lambda x, y : x // y
+
+operations = {'plus' : iadd, 'minus' : isub, 'multiplied' : imul, 'divided' : idiv}
+
+def calculate(question):
+    pass
