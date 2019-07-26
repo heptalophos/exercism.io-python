1510f8c0bee2073852ad3e4b120d42b0aa637af2
Zero Delta <zerodeltahedge@gmail.com>
2019-07-26 16:38:46 +0300

    protein-translation solution

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,2 +1,19 @@
+from itertools import takewhile
+
+CMap = (
+    (('AUG',),'Methionine'),
+    (('UUU', 'UUC'),'Phenylalanine'),
+    (('UUA', 'UUG'),'Leucine'),
+    (('UCU', 'UCC', 'UCA', 'UCG'),'Serine'),
+    (('UAU', 'UAC'),'Tyrosine'),
+    (('UGU', 'UGC'),'Cysteine'),
+    (('UGG',),'Tryptophan'),
+    (('UAA', 'UAG', 'UGA'),'STOP'))
+
+codons = { codon: amino for codons, amino in CMap for codon in codons }
+
 def proteins(strand):
-    pass
+    pchain = ["".join(c) for c in zip(*([iter(strand)] * 3))]
+    return [aminoacid for aminoacid in list(takewhile(lambda codon: codon != 'STOP', 
+                                                      [a for a in (codons[c] 
+                                                                   for c in pchain)]))]
