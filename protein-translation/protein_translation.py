53fa1e34ce9b629bb42be7e35dc0c84a99793f79
Zero Delta <zerodeltahedge@gmail.com>
2019-07-26 16:45:54 +0300

    Update protein_translation.py

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,19 +1,21 @@
 from itertools import takewhile
 
 CMap = (
-    (('AUG',),'Methionine'),
-    (('UUU', 'UUC'),'Phenylalanine'),
-    (('UUA', 'UUG'),'Leucine'),
-    (('UCU', 'UCC', 'UCA', 'UCG'),'Serine'),
-    (('UAU', 'UAC'),'Tyrosine'),
-    (('UGU', 'UGC'),'Cysteine'),
-    (('UGG',),'Tryptophan'),
-    (('UAA', 'UAG', 'UGA'),'STOP'))
+    (('AUG',), 'Methionine'),
+    (('UUU', 'UUC'), 'Phenylalanine'),
+    (('UUA', 'UUG'), 'Leucine'),
+    (('UCU', 'UCC', 'UCA', 'UCG'), 'Serine'),
+    (('UAU', 'UAC'), 'Tyrosine'),
+    (('UGU', 'UGC'), 'Cysteine'),
+    (('UGG',), 'Tryptophan'),
+    (('UAA', 'UAG', 'UGA'), 'STOP'))
 
-codons = { codon: amino for codons, amino in CMap for codon in codons }
+codons = {codon: amino for codons, amino in CMap for codon in codons}
+
 
 def proteins(strand):
     pchain = ["".join(c) for c in zip(*([iter(strand)] * 3))]
-    return [aminoacid for aminoacid in list(takewhile(lambda codon: codon != 'STOP', 
-                                                      [a for a in (codons[c] 
-                                                                   for c in pchain)]))]
+    return [aminoacid for aminoacid in list(
+                takewhile(lambda codon: codon != 'STOP',
+                                        [a for a in (codons[c]
+                                                     for c in pchain)]))]
