07857c605f1197413bca0655a86e30b39ec9a752
Zero Delta <zerodeltahedge@gmail.com>
2019-11-07 20:47:37 +0200

    Update pythagorean_triplet.py
    Additional function, solving the new specification.

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -1,40 +1,46 @@
-# def triplets_with_sum(number):
-#     if number % 4 != 0:
-#         raise ValueError('Side should be an even integer')
-#     else:
-#         triplets = []
-#         for m in range(1, number // 2 + 2):
-#             for n in range(1, m + 2):
-#                 if number == (2 * m * n ) and euclidean_conditions(m, n):
-#                     possible = [square(m) - square(n), number, square(m) + square(n)]
-#                     if is_triplet(possible):
-#                         a, b, c = sorted(possible)
-#                         triplets.append((a, b, c))
-#     return triplets
 
+## function solving the updated (different) exercise specification 
 def triplets_with_sum(number):
     triplets = []
-    for a in range(1, number // 3):
-        for b in range(1, number - a):
-            possible = [a, b + a, number - 2 * a - b]
-            if is_triplet(possible):
-                triplets.append(sorted(possible))
+    for a in range(1, int(number / 3) + 1):
+        for b in range(a + 1, int(number / 2) + 1):
+            if square(a) + square(b) < number:
+                continue
+            possible = sorted([a, b, number - a - b])
+            if is_triplet(possible) and possible not in triplets:
+                triplets.append(possible)
     return triplets
 
+
+def primitive_triplets(number_in_triplet):
+    if number_in_triplet % 2:
+        raise ValueError('Side should be an even integer')
+    triplets = set()
+    for m in range(1, number_in_triplet//2 + 2):
+        for n in range(1, m+2):
+            if (number_in_triplet == 2*m*n ) and euclidean_conditions(m, n):
+                possible = [square(m) - square(n), number_in_triplet, square(m) + square(n)]
+                if is_triplet(tuple(sorted(possible))):
+                    triplets.add(tuple(sorted(possible)))
+    return triplets
+
+
 def triplets_in_range(range_start, range_end):
-    triplets = []
+    triplets = set()
     for x in range(range_start, range_end + 1):
         for y in range(x + 1, range_end + 1):
             for z in range(y + 1, range_end + 1):
-                if is_triplet([x, y, z]):
-                    triplets.append(sorted([x, y, z]))
+                if is_triplet((x, y, z)):
+                    triplets.add(tuple(sorted((x, y, z))))
     return triplets
+
 
 def is_triplet(triplet):
     return sum([square(x) for x in triplet if x != max(triplet)]) == square(max(triplet))
 
 
 # Helpers
+
 square = lambda n: n * n
 odd = lambda n: n % 2 != 0
 within_range = lambda n1, n2: n1 > n2 > 0
@@ -42,5 +48,3 @@ hcd = lambda n1, n2: n1 if n2 == 0 else hcd(n2, n1 % n2)
 coprime = lambda n1, n2: hcd(n1, n2) == 1 
 parity = lambda n: 'odd' if odd(n) else 'even'
 euclidean_conditions = lambda x, y: within_range(x, y) and coprime(x, y) and parity(x) != parity(y)
-
-
