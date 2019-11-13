41de84dde33fae16191ae12e105266450db4ab3e
Zero Delta <zerodeltahedge@gmail.com>
2019-11-13 23:18:47 +0200

    darts solution, 1st iteration

---
+++ darts/darts.py
@@ -0,0 +1,14 @@
+square = lambda n: n * n
+sqrt = lambda n: n ** 0.5
+eudist = lambda x, y: sqrt(square(x) + square(y))
+
+def score(x, y):
+    distance = eudist(x, y)
+    if distance <= 1:
+        return 10
+    elif distance <= 5:
+        return 5
+    elif distance <= 10:
+        return 1
+    else:
+        return 0
