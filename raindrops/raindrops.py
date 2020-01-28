d09d176cc9332ce03404cbb2792c54d5084d4033
Zero Delta <zerodeltahedge@gmail.com>
2020-01-28 11:54:01 +0200

    Update raindrops.py

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,9 +1,8 @@
 def convert(number):
-    drops = ''.join(
-        sound 
-        for factor, sound in [(3, "Pling"), 
-                              (5, "Plang"), 
-                              (7, "Plong")]
-        if not number % factor
-    )
+    drops = ''.join(sound 
+                    for factor, sound in 
+                    [(3, "Pling"),
+                     (5, "Plang"), 
+                     (7, "Plong")]
+                    if not number % factor)
     return drops if drops else str(number)
