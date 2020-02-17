38435f7357b16cdcd4375f97a25f93f80e1bb0d2
Zero Delta <zerodeltahedge@gmail.com>
2020-02-17 18:47:37 +0200

    Update error_handling.py

--- error-handling/error_handling.py
+++ error-handling/error_handling.py
@@ -1,20 +1,17 @@
 def handle_error_by_throwing_exception():
     raise Exception("Exception")
 
-
 def handle_error_by_returning_none(input_data):
     try:
         return int(input_data)
     except:
         return None
 
-
 def handle_error_by_returning_tuple(input_data):
     try:
         return (True, int(input_data))
     except:
         return (False, "Invalid input")
-
 
 def filelike_objects_are_closed_on_exception(filelike_object):
     with filelike_object as flo:
