32704eb8da971478e7d70e9483dcec7cc9561052
Zero Delta <zerodeltahedge@gmail.com>
2020-02-17 18:53:52 +0200

    Update wordy.py

--- wordy/wordy.py
+++ wordy/wordy.py
@@ -1,14 +1,14 @@
 from functools import reduce
 
-iadd = lambda x, y : x + y
-isub = lambda x, y : x - y
-imul = lambda x, y : x * y
-idiv = lambda x, y : x // y
+wadd = lambda x, y : x + y
+wsub = lambda x, y : x - y
+wmul = lambda x, y : x * y
+wdiv = lambda x, y : x // y
 
-validOps = {'plus'       : iadd, 
-            'minus'      : isub, 
-            'multiplied' : imul, 
-            'divided'    : idiv }
+validOps = {'plus'       : wadd, 
+            'minus'      : wsub, 
+            'multiplied' : wmul, 
+            'divided'    : wdiv }
 
 def curry(func, var):
     y = var
@@ -16,16 +16,15 @@ def curry(func, var):
         return func(x, y)
     return f
 
-def calculate(question):
+def answer(question):
     question = question.lstrip('What is').rstrip('?')
-    tokens = [t if t in validOps else int(t) 
-              for t in question.split(' ') if t != 'by']
+    tokens = [t for t in question.split(' ') if t != 'by']
     if len(tokens) == 0:
        raise ValueError('invalid question: no numbers here') 
     if len(tokens) == 1:
         return int(tokens[0])
     try:
-        curries = [curry(validOps[op], num) 
+        curries = [curry(validOps[op], int(num)) 
                    for (op, num) in zip(tokens[1::2], tokens[2::2])]
         if (len(curries) * 2 != len(tokens) - 1):
             raise ValueError('invalid question: operands not matched')
