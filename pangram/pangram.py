095786eb18b65a7d2d550477adcb2470953beeff
Zero Delta <zerodeltahedge@gmail.com>
2020-03-18 23:57:03 +0200

    Update pangram.py

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -2,7 +2,9 @@ import re
 
 dict = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-def normal(text): return re.sub(r'\W+|_|\d+', '', text)
+def normal(text): 
+    return re.sub(r'\W+|_|\d+', '', text)
 
 def is_pangram(text):
-    return set(normal(text).lower()) == set(dict.lower())
+    return set(normal(text).lower()) 
+           == set(dict.lower())
