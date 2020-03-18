dea9a85ce9736d37b3199a77971d4da91d2ec923
Zero Delta <zerodeltahedge@gmail.com>
2020-03-18 23:54:21 +0200

    Update isbn_verifier.py

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,8 +1,13 @@
-def verify(isbn):
-    digits = list(isbn.replace('-', '').replace('_', ''))
+def is_valid(isbn):
+    digits = list(isbn.replace('-', '')
+                      .replace('_', ''))
+    existnondigs = any(not d.isdigit() 
+                       for d in digits)  
     if digits and digits[-1] == 'X':
         digits[-1] = '10'
-    if len(digits) != 10 or any(not d.isdigit() for d in digits):
+    if len(digits) != 10 or existnondigs:
         return False
     countdown = list(range(10, 0, -1))
-    return sum(int(d) * c for d, c in zip(digits, countdown)) % 11 == 0
+    csum = sum(int(d) * c  
+               for d, c in zip(digits, countdown))  
+    return  csum % 11 == 0
