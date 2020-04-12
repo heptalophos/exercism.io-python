6dc2a22bfd05c9bdc246787772bac5b2b4349469
Zero Delta <zerodeltahedge@gmail.com>
2020-04-12 23:12:19 +0300

    Reformat anagram.py

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,7 +1,8 @@
-def detect_anagrams(word, candidates):
+def find_anagrams(word, candidates):
     """
-    detect which candidates members are anagrams of word
+    detect which candidates 
+    are anagrams of word
     """
-    return [candidate for candidate in candidates if sorted(candidate.lower()) == sorted(word.lower())
-                                                     and candidate.lower() != word.lower()
-           ]
+    return [candidate for candidate in candidates 
+            if sorted(candidate.lower()) == sorted(word.lower())
+               and candidate.lower() != word.lower()]
