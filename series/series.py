29c3653e1d47ce94ec45ac8acfef5e6edaf880e5
Zero Delta <zerodeltahedge@gmail.com>
2020-04-21 21:52:41 +0300

    Refactor series (updated spec)

--- series/series.py
+++ series/series.py
@@ -1,5 +1,5 @@
 def slices(series, length):
-    digits = [int(d) for d in series]
-    if len(digits) < length or length < 1:
+    if len(series) < length or length < 1:
         raise ValueError("Length not appropriate")
-    return [digits[n : n + length] for n in range(len(digits) + 1 - length)]
+    return [series[n : n + length] 
+            for n in range(len(series) + 1 - length)]
