296637d5aeadcfc3ae35332f3a018d6862b83ace
Zero Delta <zerodeltahedge@gmail.com>
2020-05-14 23:33:48 +0300

    Refactor grains

--- grains/grains.py
+++ grains/grains.py
@@ -1,16 +1,20 @@
 import functools
 
-def on_square(integer_number):
+ERROR = "Invalid input. Range is (1, 64)"
+
+def square(integer_number):
     if valid_input(integer_number):
         return 2 ** (integer_number - 1)
     else:
-        raise ValueError("Invalid input. Range is (1, 64)")
+        raise ValueError(ERROR)
 
-def total_after(integer_number):
+def total():
+    integer_number = 64
     if valid_input(integer_number):
-        return sum(map(on_square, range(1, integer_number + 1)))
+        return sum(map(square, 
+                       range(1, integer_number + 1)))
     else:
-        raise ValueError("Invalid input. Range is (1, 64)")
+        raise ValueError(ERROR)
 
 def valid_input(integer_number):
     if (integer_number < 1) or (integer_number > 64):
