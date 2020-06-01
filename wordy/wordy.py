4fd8c84e89dc349ad62a012105c6357c01ba492a
Zero Delta <zerodeltahedge@gmail.com>
2020-06-01 21:29:36 +0300

    Update wordy.py

--- wordy/wordy.py
+++ wordy/wordy.py
@@ -18,16 +18,22 @@ def curry(func, var):
 
 def answer(question):
     question = question.lstrip('What is').rstrip('?')
-    tokens = [t for t in question.split(' ') if t != 'by']
+    tokens = [t for t 
+                in question.split(' ') if t != 'by']
     if len(tokens) == 0:
        raise ValueError('invalid question: no numbers here') 
     if len(tokens) == 1:
         return int(tokens[0])
     try:
         curries = [curry(validOps[op], int(num)) 
-                   for (op, num) in zip(tokens[1::2], tokens[2::2])]
+                   for (op, num) 
+                   in zip(tokens[1::2], tokens[2::2])]
         if (len(curries) * 2 != len(tokens) - 1):
-            raise ValueError('invalid question: operands not matched')
+            raise ValueError('invalid question:'
+                             ' operands not matched')
     except:
             raise ValueError('invalid sequence of operands')
-    return reduce (lambda num, op: op(num), curries, int(tokens[0]))
+    return reduce (lambda num, op: 
+                   op(num), 
+                   curries, 
+                   int(tokens[0]))
