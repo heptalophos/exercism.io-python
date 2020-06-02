ad294a680fa7fa4a1f5fa8f80de1fc14e100cfc7
Zero Delta <zerodeltahedge@gmail.com>
2020-06-02 23:04:32 +0300

    Update rational_numbers.py

--- rational-numbers/rational_numbers.py
+++ rational-numbers/rational_numbers.py
@@ -1,11 +1,13 @@
 from __future__ import division
 class Rational(object):
     def __init__(self, numer, denom):
-        self.numer, self.denom = Rational.lowest_form(numer, denom)
+        self.numer, self.denom = Rational.lowest_form(numer, \
+                                                      denom)
 
     def __eq__(self, other):
         if isinstance(other, Rational):
-            return self.numer == other.numer and self.denom == other.denom
+            return self.numer == other.numer and \
+                   self.denom == other.denom
         else:
             return (self.numer / self.denom) == other
 
@@ -13,28 +15,36 @@ class Rational(object):
         return '{}/{}'.format(self.numer, self.denom)
 
     def __add__(self, other):
-        return Rational(self.numer * other.denom + other.numer * self.denom, self.denom * other.denom)
+        return Rational(self.numer * other.denom + \
+                        other.numer * self.denom, \
+                        self.denom * other.denom)
 
     def __sub__(self, other):
-        return Rational(self.numer * other.denom - other.numer * self.denom, self.denom * other.denom)
+        return Rational(self.numer * other.denom - \
+                        other.numer * self.denom, \
+                        self.denom * other.denom)
 
     def __mul__(self, other):
-        return Rational(self.numer * other.numer, self.denom * other.denom)
+        return Rational(self.numer * other.numer, \
+                        self.denom * other.denom)
 
     def __truediv__(self, other):
         if other.numer * self.denom == 0:
             raise ValueError("Division by zero")
-        return Rational(self.numer * other.denom, self.denom * other.numer)
+        return Rational(self.numer * other.denom, \
+                        self.denom * other.numer)
 
     def __abs__(self):
         return  Rational(abs(self.numer), abs(self.denom))
 
     def __pow__(self, power):
         if power >= 0 or isinstance(power, float):
-            return Rational(self.numer ** power, self.denom ** power)
+            return Rational(self.numer ** power, \
+                            self.denom ** power)
         else:
             power = abs(power)
-            return Rational(self.denom ** power, self.numer ** power)
+            return Rational(self.denom ** power, \
+                            self.numer ** power)
 
     def __rpow__(self, base):
         power = self.numer / self.denom
@@ -44,7 +54,8 @@ class Rational(object):
     def lowest_form (numer, denom):
         if denom == 0:
             raise ValueError("Denominator cannot be zero")
-        return (numer / Rational.gcd(numer, denom), denom / Rational.gcd(numer, denom)) 
+        return (numer / Rational.gcd(numer, denom), \
+                denom / Rational.gcd(numer, denom)) 
 
     @staticmethod
     def gcd(numer, denom):
