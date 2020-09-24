b7ee777afbb1962cf0ccbe92a9cedc4566f9ca3d
Zero Delta <zerodeltahedge@gmail.com>
2020-09-24 21:12:02 +0300

    Update perfect_numbers.py

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,2 +1,18 @@
+def pfactors(number):
+    if number == 1:
+        yield 1
+        return
+    for i in range(2, number + 1):
+        if number % i == 0:
+            yield i 
+            yield from pfactors(number // k)
+                 
+def aliquot(number):
+    return number == sum(pfactors(number))
+
 def classify(number):
-    pass
+    if (number < 1):
+        raise ValueError("Not a natural")
+    return "abundant" if aliquot(number) > number \
+            else "perfect" if aliquot(number) == number \
+            else "deficient"
