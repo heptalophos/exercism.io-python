bceef085ff7149d325721934e0c02f8e48611da8
Zero Delta <zerodeltahedge@gmail.com>
2020-12-21 18:39:04 +0200

    Update high_scores.py

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,8 +1,8 @@
 def latest(scores):
-    return scores[-1]
+    return scores[ -1]
 
 def personal_best(scores):
     return max(scores)
 
 def personal_top_three(scores):
-    return sorted(scores, reverse=True)[:3]
+    return sorted(scores, reverse = True)[ :3]
