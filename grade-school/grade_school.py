ba575aec5701be04f98252ac21dd893f201ba68a
Zero Delta <zerodeltahedge@gmail.com>
2020-12-22 23:52:55 +0200

    Update grade_school.py

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,18 +1,17 @@
 from collections import defaultdict
 
 class School(object):
-    
     def __init__(self):
         self.school = defaultdict(set)
-        
+        self.grades = self.school.keys()
+
     def add_student(self, name, grade):
         self.school[grade].add(name)
-        
+
     def grade(self, grade):
         return list(sorted(self.school[grade]))
 
     def roster(self): 
-        grades = self.school.keys()
         return [name
-                for grade in sorted(grades)
+                for grade in sorted(self.grades)
                 for name in self.grade(grade)]
