3735fdf14294b3dab606fc30ac499505e033fc3c
Zero Delta <zerodeltahedge@gmail.com>
2021-01-08 22:27:00 +0200

    Update matching_brackets.py

--- matching-brackets/matching_brackets.py
+++ matching-brackets/matching_brackets.py
@@ -1,10 +1,10 @@
 brackets = dict(zip('({[', ')}]'))
 
 def is_paired(input_string):
-    normalised = ''.join(c for c in input_string 
-                           if c in '(){}[]')
+    just_brackets = ''.join(c for c in input_string 
+                              if c in '(){}[]')
     stack = []
-    for c in normalised:
+    for c in just_brackets:
         if c in brackets:
             stack.append(brackets[c])
         elif not stack or c != stack.pop():
