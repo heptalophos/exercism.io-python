cde76a81e82092cfc00b4a3a7afba981851d92b2
Zero Delta <zerodeltahedge@gmail.com>
2021-01-14 23:20:55 +0200

    Refactor leap

--- leap/leap.py
+++ leap/leap.py
@@ -1,3 +1,4 @@
 def leap_year(year):
-    return (((year % 4 == 0) and (year % 100 != 0))
-           or (year % 400 == 0))
+    return year % 400 == 0 \
+           if year % 100 == 0 \
+           else year % 4 == 0 
