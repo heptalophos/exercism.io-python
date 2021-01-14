2efbd1b8059abe76aba352f3167aafc886a1aab6
Zero Delta <zerodeltahedge@gmail.com>
2021-01-14 23:04:24 +0200

    Refactor bob.py

--- bob/bob.py
+++ bob/bob.py
@@ -1,16 +1,20 @@
-normal = lambda x : x.strip()
-silent = lambda x : not x
-shout = lambda x : x.isupper() 
-question = lambda x : x.endswith('?')
+sanitized = lambda x : x.strip()
+silent    = lambda x : not x
+shout     = lambda x : x.isupper() 
+question  = lambda x : x.endswith('?')
 
 def response(text):
-    text = normal(text)
+    text = sanitized(text)
     if silent(text):
-        return "Fine. Be that way!"
+        return \
+        "Fine. Be that way!"
     if shout(text) and question(text):
-        return "Calm down, I know what I'm doing!"
+        return \
+        "Calm down, I know what I'm doing!"
     if shout(text):
-        return "Whoa, chill out!"
+        return \
+        "Whoa, chill out!"
     if question(text):
-        return "Sure."
+        return \
+        "Sure."
     return "Whatever."
