d5a990ce041714d9de26d75021153aaa247945dd
Zero Delta <zerodeltahedge@gmail.com>
2021-02-01 22:59:53 +0200

    Update yacht.py

--- yacht/yacht.py
+++ yacht/yacht.py
@@ -1,6 +1,5 @@
 from collections import Counter
 
-
 counts = lambda n : lambda dice : n * Counter(dice)[n]
 
 def str8(dice):
@@ -12,7 +11,8 @@ def str8(dice):
 # Score categories
 # Change the values as you see fit
 YACHT = lambda dice: (50 
-            if len(dice) == 5 and len(set(dice)) == 1 
+            if len(dice) == 5 and 
+               len(set(dice)) == 1 
             else 0)
 ONES = counts(1)
 TWOS = counts(2)
@@ -21,10 +21,14 @@ FOURS = counts(4)
 FIVES = counts(5)
 SIXES = counts(6)
 FULL_HOUSE = lambda dice : (sum(dice) 
-                if sorted(tuple(Counter(dice).values())) == [2, 3] 
+                if sorted(tuple(
+                    Counter(dice).values()
+                         )
+                   ) == [2, 3] 
                 else 0)
 FOUR_OF_A_KIND = lambda dice : [(4*key if val >= 4 else 0) 
-                    for key, val in Counter(dice).items()][0]
+                                for key, val 
+                                in Counter(dice).items()][0]
 LITTLE_STRAIGHT = lambda dice : (30 
                     if str8(dice) == 5 and max(dice) == 5 
                     else 0)
