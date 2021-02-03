92487f1db848cfcdb3337fb1da68486a9c6c82bc
Zero Delta <zerodeltahedge@gmail.com>
2021-02-03 17:21:59 +0200

    Refactor saddle_points.py

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -4,7 +4,7 @@ def saddle_points(matrix):
     if any(len(row) != len(matrix[0]) for row in matrix):
             raise ValueError("irregular matrix")
     columns = list(zip(*matrix))
-    return [{'row': rx + 1, 'column': cx + 1} 
+    return [{"row": rx + 1, "column": cx + 1} 
             for rx, row in enumerate(matrix)
             for cx, col in enumerate(row)
             if min(columns[cx]) == max(row)]
