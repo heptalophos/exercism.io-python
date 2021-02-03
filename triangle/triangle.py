46955cc94c3788779a22e2a61305e11a71614236
Zero Delta <zerodeltahedge@gmail.com>
2021-02-03 17:10:35 +0200

    Refactor triangle.py

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,14 +1,16 @@
-triangle_inequality = lambda sides: \
-                2 * max(sides) < sum(sides)
+triangle_inequality = lambda f: \
+                      lambda sides: \
+            2 * max(sides) < sum(sides) \
+            and f(sides)
 
+@triangle_inequality
 def equilateral(sides):
-    return len(set(sides)) == 1 and \
-           triangle_inequality(sides)
+    return len(set(sides)) == 1
 
+@triangle_inequality
 def isosceles(sides):
-    return len(set(sides)) <= 2 and \
-           triangle_inequality(sides)
+    return len(set(sides)) <= 2
 
+@triangle_inequality
 def scalene(sides):
-    return len(set(sides)) == 3 and \
-           triangle_inequality(sides)
+    return len(set(sides)) == 3
