98e5cd8bf90dde98000ba515f7f16eabf8d9e324
Zero Delta <zerodeltahedge@gmail.com>
2021-02-03 17:03:31 +0200

    Create triangle.py

---
+++ triangle/triangle.py
@@ -0,0 +1,14 @@
+triangle_inequality = lambda sides: \
+                2 * max(sides) < sum(sides)
+
+def equilateral(sides):
+    return len(set(sides)) == 1 and \
+           triangle_inequality(sides)
+
+def isosceles(sides):
+    return len(set(sides)) <= 2 and \
+           triangle_inequality(sides)
+
+def scalene(sides):
+    return len(set(sides)) == 3 and \
+           triangle_inequality(sides)
