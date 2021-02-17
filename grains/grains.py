1f8eeb1c1728c5f9b9a6e70f10c6366ed8a1aa27
Zero Delta <zerodeltahedge@gmail.com>
2021-02-17 16:59:44 +0200

    Reformat grains

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,10 @@
-import functools
+def valid_input(integer_number):
+    if (integer_number < 1) or (integer_number > 64):
+        return False
+    return True
 
 ERROR = "Invalid input. Range is (1, 64)"
+
 
 def square(integer_number):
     if valid_input(integer_number):
@@ -11,7 +15,3 @@ def square(integer_number):
 def total():
     return (1 << 64) - 1
 
-def valid_input(integer_number):
-    if (integer_number < 1) or (integer_number > 64):
-        return False
-    return True
