27e24a68478863f28e2a46e6f6fea8b117566674
Zero Delta <zerodeltahedge@gmail.com>
2021-03-30 16:28:21 +0300

    Refactor robot-name

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,5 +1,4 @@
 def sum_of_multiples(limit, multiples):
-    return sum( set(value 
-                    for m in multiples 
-                    if not m == 0 
-                    for value in range(0, limit, m)))
+    return sum(set(x for m in multiples 
+                   if not m == 0 
+                   for x in range(0, limit, m)))
