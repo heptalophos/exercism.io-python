75717624e07704eccaff92013f8ad0dfd6f69144
Zero Delta <zerodeltahedge@gmail.com>
2021-03-31 16:39:52 +0300

    Update sum_of_multiples.py

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,4 +1,5 @@
 def sum_of_multiples(limit, multiples):
-    return sum(set(x for m in multiples 
-                   if not m == 0 
-                   for x in range(0, limit, m)))
+    return sum(
+            set(x for m in multiples 
+                  if not m == 0
+                  for x in range(0, limit, m)))
