a0372b5ff0b06659e3a0223d79dec6b259f0312a
Zero Delta <zerodeltahedge@gmail.com>
2021-05-29 01:00:23 +0300

    Refactor binary_search_tree.py

--- binary-search-tree/binary_search_tree.py
+++ binary-search-tree/binary_search_tree.py
@@ -1,15 +1,4 @@
-class TreeNode(object):
-    def __init__(self, data, left, right):
-        self.data = data
-        self.left = left
-        self.right = right
-
-    def __str__(self):
-        fmt = 'TreeNode(data={}, left={}, right={})'
-        return fmt.format(self.data, \
-                          self.left, \
-                          self.right)
-
+from tree_node import TreeNode
 
 class BinarySearchTree(object):
     def __init__(self, tree_data):
