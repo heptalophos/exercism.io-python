73b7bd749c88d2b2e9b142a8f14fb81561c9b408
Zero Delta <zerodeltahedge@gmail.com>
2021-09-12 14:46:10 +0300

    Refactor phone-number

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -2,22 +2,22 @@ class PhoneNumber(object):
     def __init__(self, num):
         INVALID = '0' * 10
         digits = [d for d in num if d.isdigit()]
-        if len(digits) == 11 and digits[0] != '1':
-            raise ValueError("Invalid number")
+        if len(digits) == 11: 
+            if digits[0] != '1':
+                raise ValueError("11 digits, not starting with 1")
+            else:
+                digits = digits[1:]
         if len(digits) != 10:
-            raise ValueError("Invalid number")
-        
+            raise ValueError("Number over or less than 10 digits")
         self.number = ''.join(digits[-10:]) or INVALID
-
-    def area_code(self):
         if self.number[0] < '2' or self.number[3] < '2':
-            raise ValueError("Invalid number")
-        return self.number[:3]
+            raise ValueError("Invalid area code or exchange code")
+        self.area_code = self.number[:3]
 
     def pretty(self):
         return '({}) {}-{}'.format(
             self.area_code,
-            self.number[3:6], 
-            self.number[6:]
+            self.number[-7:-4], 
+            self.number[-4:]
         )
         
