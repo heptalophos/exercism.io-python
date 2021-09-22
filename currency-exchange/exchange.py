0a501fb18e240774d45acd0d3043aec8b1a6e051
Zero Delta <zerodeltahedge@gmail.com>
2021-09-22 14:28:04 +0300

    Update exchange.py

--- currency-exchange/exchange.py
+++ currency-exchange/exchange.py
@@ -1,6 +1,5 @@
 def estimate_value(budget, exchange_rate):
     """
-
     :param budget: float - amount of money you are planning to exchange.
     :param exchange_rate: float - unit value of the foreign currency.
     :return: float - estimated value of the foreign currency you can receive
@@ -10,7 +9,6 @@ def estimate_value(budget, exchange_rate):
 
 def get_change(budget, exchanging_value):
     """
-
     :param budget: float - amount of money you own.
     :param exchanging_value: int - amount of your money you want to exchange now.
     :return: float - amount left of your starting currency after exchanging
@@ -20,7 +18,6 @@ def get_change(budget, exchanging_value):
 
 def get_value(denomination, number_of_bills):
     """
-
     :param denomination: int - the value of a bill.
     :param number_of_bills: int - amount of bills you received.
     :return: int - total value of bills you now have
@@ -30,7 +27,6 @@ def get_value(denomination, number_of_bills):
 
 def get_number_of_bills(budget, denomination):
     """
-
     :param budget: float - the amount of money you are planning to exchange.
     :param denomination: int - the value of a single bill.
     :return: int - number of bills after exchanging all your money
@@ -40,7 +36,6 @@ def get_number_of_bills(budget, denomination):
 
 def exchangeable_value(budget, exchange_rate, spread, denomination):
     """
-
     :param budget: float - the amount of your money you are planning to exchange.
     :param exchange_rate: float - the unit value of the foreign currency.
     :param spread: int - percentage that is taken as an exchange fee.
@@ -55,7 +50,6 @@ def exchangeable_value(budget, exchange_rate, spread, denomination):
 
 def unexchangeable_value(budget, exchange_rate, spread, denomination):
     """
-
     :param budget: float - the amount of your money you are planning to exchange.
     :param exchange_rate: float - the unit value of the foreign currency.
     :param spread: int - percentage that is taken as an exchange fee.
