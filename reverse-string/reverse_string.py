036cf7965cc5b3c1fa653703de4f6dc8a17d2b7d
Zero Delta <zerodeltahedge@gmail.com>
2021-09-27 00:18:09 +0300

    Refactor reverse-string

--- reverse-string/reverse_string.py
+++ reverse-string/reverse_string.py
@@ -1,7 +1,2 @@
 def reverse(string):
-    if len(string) == 1 or len(string) == 0:
-        return string
-    else:
-        return "{}{}{}".format(string[-1], 
-                               reverse(string[1:-1]), 
-                               string[0])
+    return (len(string) > 1 and reverse(string[1:]) + string[0]) or string
