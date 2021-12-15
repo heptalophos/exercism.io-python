95aab379bd1f37f70b4acb9143c7ad723057e5d9
Zero Delta <zerodeltahedge@gmail.com>
2021-12-15 23:25:07 +0200

    Update complex_numbers.py

--- complex-numbers/complex_numbers.py
+++ complex-numbers/complex_numbers.py
@@ -5,37 +5,40 @@ class ComplexNumber(object):
         self.real = real
         self.imaginary = imaginary
 
+    def _ensure_complex(f):
+        def wrapper(self, n): 
+            if not isinstance(n, ComplexNumber):
+                number = ComplexNumber(n, 0)
+            return f(self, n)
+        return wrapper
+
+    @_ensure_complex
     def __add__(self, other):
         r = self.real + other.real
         i = self.imaginary + other.imaginary
         return ComplexNumber(r, i)
 
+    @_ensure_complex
     def __mul__(self, other):
-        r = self.real * other.real - \
-            self.imaginary * other.imaginary
-        i = self.imaginary * other.real + \
-            self.real * other.imaginary
+        r = self.real * other.real - self.imaginary * other.imaginary
+        i = self.imaginary * other.real + self.real * other.imaginary
         return ComplexNumber(r, i)
 
+    @_ensure_complex
     def __sub__(self, other):
         r = self.real - other.real
         i = self.imaginary - other.imaginary
         return ComplexNumber(r, i)
 
+    @_ensure_complex
     def __truediv__(self, other):
-        denominator = other.real ** 2 + \
-                      other.imaginary ** 2
-        realnumerator = self.real * other.real + \
-                self.imaginary * other.imaginary 
-        imagnumerator = self.imaginary * other.real - \
-                self.real * other.imaginary
-        return ComplexNumber(
-            realnumerator / denominator, 
-            imagnumerator / denominator)
+        denom = other.real ** 2 + other.imaginary ** 2
+        realn = self.real * other.real + self.imaginary * other.imaginary 
+        imagn = self.imaginary * other.real - self.real * other.imaginary
+        return ComplexNumber(realn / denom, imagn / denom)
 
     def __abs__(self):
-        return math.sqrt(self.real ** 2 + \
-                         self.imaginary ** 2)
+        return math.sqrt(self.real ** 2 + self.imaginary ** 2)
 
     def conjugate(self):
         return ComplexNumber(self.real, -self.imaginary)
@@ -43,10 +46,8 @@ class ComplexNumber(object):
     def exp(self):
         xr = math.exp(self.real)
         i = self.imaginary
-        return ComplexNumber(
-                    xr * math.cos(i),
-                    xr * math.sin(i))
+        return ComplexNumber(xr * math.cos(i), xr * math.sin(i))
 
+    @_ensure_complex
     def __eq__(self, other):
-        return self.real == other.real and \
-               self.imaginary == other.imaginary
+        return self.real == other.real and self.imaginary == other.imaginary
