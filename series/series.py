eac53e7cb6de81cba6b7005d61c296b80e00dba8
Zero Delta <zerodeltahedge@gmail.com>
2022-01-20 23:18:12 +0200

    Update series.py

--- series/series.py
+++ series/series.py
@@ -1,5 +1,6 @@
 def slices(series, length):
     if len(series) < length or length < 1:
         raise ValueError("Length not appropriate")
-    return [series[n : n + length] for n in 
-            range(len(series) + 1 - length)]
+    return [series[n : n + length] 
+            for n in range(len(series) + 1 - length)]
+    
