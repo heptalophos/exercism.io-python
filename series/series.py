3820a201386ce90e1a150b10a40ad649cd6787f0
Zero Delta <zerodeltahedge@gmail.com>
2022-01-20 23:24:25 +0200

    Refactor series

--- series/series.py
+++ series/series.py
@@ -1,5 +1,11 @@
 def slices(series, length):
-    if len(series) < length or length < 1:
+    if series == "":
+        raise ValueError("series cannot be empty")
+    if length < 0:
+        raise ValueError("slice length cannot be negative")
+    if length < 1:
+        raise ValueError("slice length cannot be zero")
+    if len(series) < length:
         raise ValueError("slice length cannot be greater than series length")
     return [series[n : n + length] for n in range(len(series) + 1 - length)]
     
