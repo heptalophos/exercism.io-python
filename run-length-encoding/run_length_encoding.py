4ed687db69068981c01e03cda3c6de4762b26191
Zero Delta <zerodeltahedge@gmail.com>
2022-02-11 11:41:38 +0200

    Refactor run-length-encoding

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,16 +1,13 @@
 from re import sub
 from itertools import groupby
 
-
 def decode(text):
-    return sub(r'(\d+)(\D)', 
-               lambda m: m.group(2) * int(m.group(1)), 
-               text)
+    dec = lambda m: m.group(2) * int(m.group(1))
+    return sub(r'(\d+)(\D)', dec, text)
 
 def encode(text):
-   return ''.join(x if cnt <= 1 
-                    else "%d%s" % (cnt, x) 
-                  for x, cnt 
-             in map(lambda x: (x[0], 
-                               len(list(x[1]))), 
-                               groupby(text)))
+    enc = (x if cnt <= 1  
+             else "%d%s" % (cnt, x) 
+           for x, cnt in map(lambda x: (x[0], len(list(x[1]))), 
+                             groupby(text)))
+    return ''.join(enc)
