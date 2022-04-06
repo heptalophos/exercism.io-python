40a996d64d4c479b37a76cac92f278033993bce0
Zero Delta <zerodeltahedge@gmail.com>
2022-04-06 18:09:34 +0300

    Refactor largest_series_product.py

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -2,26 +2,19 @@ import re
 from functools import reduce
 
 errors = [ 
-    "Series size cannot be negative",
-    "String should contain only digits",
-    "Series size cannot be larger than containing string's length"
+    "span must not be negative",
+    "digits input must only contain digits",
+    "span must be smaller than string length"
     ]
 
 def largest_product(series, size):
-    
     if size < 0:
         raise ValueError(errors[0])
-    
     if len(re.sub("[^0-9]+", "", series)) != len(series):
         raise ValueError(errors[1])
-    
     if size > len(series):
         raise ValueError(errors[2])
-
-    slices = [series[i:i + size] 
-              for i in range(len(series) - size + 1)]
-    prods  = [[prods 
-              for prods in map(int, list(s))] 
-              for s in slices]
-    return max([reduce (lambda x, y: x * y, n, 1) 
-                for n in prods])
+    slices = [series[i:i + size] for i in range(len(series) - size + 1)]
+    slice_vectors = [[d for d in map(int, list(s))] for s in slices]
+    products = [reduce (lambda x, y: x * y, v, 1) for v in slice_vectors]
+    return max(products)
