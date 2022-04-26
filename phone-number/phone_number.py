43db65ccd6f2ddcab754c1227d34dc86243fca92
Zero Delta <zerodeltahedge@gmail.com>
2022-04-26 23:43:58 +0300

    Update phone_number.py

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -2,23 +2,24 @@ class PhoneNumber(object):
     def __init__(self, num):
         INVALID = '0' * 10
         digits = [d for d in num if d.isdigit()]
-        if len(digits)
+        if len(digits) < 10:
+            raise ValueError("incorrect number of digits")
+        if len(digits) > 11:
+            raise ValueError("more than 11 digits")
         if len(digits) == 11: 
             if digits[0] != '1':
                 raise ValueError("11 digits must start with 1")
             else:
                 digits = digits[1:]
-        if len(digits) != 10:
-            raise ValueError("Number over or less than 10 digits")
         self.number = ''.join(digits[-10:]) or INVALID
         if self.number[0] == '0' :
-            raise ValueError("exchange code cannot start with zero")
-        if self.number[0] < '2' :
-            raise ValueError("exchange code cannot start with one")
-        if self.number[3] == '0' :
             raise ValueError("area code cannot start with zero")
-        if self.number[3] < '2' :
+        if self.number[0] < '2' :
             raise ValueError("area code cannot start with one")
+        if self.number[3] == '0' :
+            raise ValueError("exchange code cannot start with zero")
+        if self.number[3] < '2' :
+            raise ValueError("exchange code cannot start with one")
         self.area_code = self.number[:3]
 
     def pretty(self):
