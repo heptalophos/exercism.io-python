b60b11c8bb17bc94fe8c2ab20f68c9c902dd714c
Zero Delta <zerodeltahedge@gmail.com>
2022-05-04 20:13:04 +0300

    Update strings.py

--- little-sisters-vocab/strings.py
+++ little-sisters-vocab/strings.py
@@ -8,7 +8,7 @@ def add_prefix_un(word):
     :return: str - of root word prepended with 'un'.
     """
 
-    pass
+    return f'un{word}'
 
 
 def make_word_groups(vocab_words):
@@ -26,7 +26,7 @@ def make_word_groups(vocab_words):
     produces the following string: 'en :: enclose :: enjoy :: enlighten'.
     """
 
-    pass
+    return f' :: {vocab_words[0]}'.join(vocab_words)
 
 
 def remove_suffix_ness(word):
@@ -38,7 +38,9 @@ def remove_suffix_ness(word):
     For example: "heaviness" becomes "heavy", but "sadness" becomes "sad".
     """
 
-    pass
+    if word[-5] == 'i':
+        return f'{word[:-5]}y'
+    return word[:-4]
 
 
 def adjective_to_verb(sentence, index):
@@ -51,4 +53,5 @@ def adjective_to_verb(sentence, index):
     For example, ("It got dark as the sun set", 2) becomes "darken".
     """
 
-    pass
+    word_array = sentence.replace(".", "").split(' ')
+    return f'{word_array[index]}en'
