d9f91d80f6ecea636054d18e5ea506d78e85fc3d
Zero Delta <zerodeltahedge@gmail.com>
2022-06-15 16:27:32 +0300

    Refactor secret-handshake

--- secret-handshake/secret_handshake.py
+++ secret-handshake/secret_handshake.py
@@ -1,16 +1,8 @@
-_actions = ["wink", "double blink", "close your eyes", "jump"]
+actions = ["wink", "double blink", "close your eyes", "jump"]
 
 def commands(code):
-    seq = [a for i, a in enumerate(_actions) if code & 1 << i]
+    code = int(code, 2)
+    handshake = [a for i, a in enumerate(actions) if code & 1 << i]
     if code & 1 << 4:
-        seq = list(reversed(seq))
-    return seq
-
-def secret_code(actions):
-    seq = actions
-    if not set(actions).issubset(set(_actions)):
-        seq = []
-    code = sum(1 << _actions.index(a) for a in _actions)  
-    if commands(code) != seq:
-        code += 1 << 4
-    return code 
+        handshake = list(reversed(handshake))
+    return handshake
