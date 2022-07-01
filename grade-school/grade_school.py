00ebdbcd52a70ccf0a0df9f1badec9322b00bbf7
Zero Delta <zerodeltahedge@gmail.com>
2022-07-01 16:46:52 +0300

    Refactor grade-school

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,17 +1,28 @@
-from collections import defaultdict
-
 class School(object):
     def __init__(self):
-        self.school = defaultdict(set)
-        self.grades = self.school.keys()
+        self.school   = {}
+        self.enrolled = []
 
     def add_student(self, name, grade):
-        self.school[grade].add(name)
+        if name in self.roster():
+            self.enrolled.append(False)
+        else:
+            if not grade in self.school.keys():
+                self.school[grade] = []
+            self.school[grade].append(name)
+            self.enrolled.append(True)
 
     def grade(self, grade):
-        return list(sorted(self.school[grade]))
+        return sorted(self.school.get(grade, []))
 
     def roster(self): 
-        return [name
-                for grade in sorted(self.grades)
-                for name in self.grade(grade)]
+        roster = []
+        if len(self.school.keys()) > 0:
+            grades = [g for (g, _) in sorted(self.school.items())]
+            for grade in grades:
+                students = [s for s in sorted(self.school[grade])]
+                roster.extend(students)
+        return roster
+
+    def added(self):
+        return self.enrolled
