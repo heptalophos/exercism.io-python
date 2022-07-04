495e4b8474514138fc8e69747cbcae0db6dce076
Zero Delta <zerodeltahedge@gmail.com>
2022-07-04 13:56:37 +0300

    Refactor high-scores

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,14 +1,12 @@
 class HighScores(object):
-    
     def __init__(self, scores):
         self.scores = scores
 
     def latest(self):
         return self.scores[-1]
 
-    def personal_best(self):
-        return max(self.scores)
-
     def personal_top_three(self):
-        return sorted(self.scores, reverse=True)[:3]
-    
+        return sorted(self.scores, reverse = True)[:3]
+
+    def personal_best(self):
+        return self.personal_top_three()[0]    
