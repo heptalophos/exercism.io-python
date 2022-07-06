286647c8b3ce76844109e938f28e6b5c9a28ca5a
Zero Delta <zerodeltahedge@gmail.com>
2022-07-06 18:19:57 +0300

    Refactor matching_brackets.py

--- matching-brackets/matching_brackets.py
+++ matching-brackets/matching_brackets.py
@@ -1,10 +1,9 @@
 brackets = dict(zip('({[', ')}]'))
 
 def is_paired(input_string):
-    just_brackets = ''.join(c for c in input_string 
-                              if c in '(){}[]')
     stack = []
-    for c in just_brackets:
+    just_brackets = ( c for c in input_string if c in '(){}[]')
+    for c in ''.join(just_brackets):
         if c in brackets:
             stack.append(brackets[c])
         elif not stack or c != stack.pop():
