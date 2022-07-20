96e5949f0e5b483110c9d481fa412730345b26a4
Zero Delta <zerodeltahedge@gmail.com>
2022-07-20 18:37:44 +0300

    Update atbash_cipher.py

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,16 +1,12 @@
 from string import ascii_lowercase as al
 
-translation = lambda t : \
-    t.translate(str.maketrans(al, al[::-1]))
+translation = lambda t : t.translate(str.maketrans(al, al[::-1]))
 
 def encode(plain_text):
-    normalized = [c for c in plain_text 
-                    if c.isalnum()]
-    enc = translation(''.join(normalized).lower())
-    return " ".join([enc[i:i + 5] 
-                    for i in range(0, len(enc), 5)])
+    normalized = [c for c in plain_text if c.isalnum()]
+    encoded = translation(''.join(normalized).lower())
+    return " ".join([encoded[i:i + 5] for i in range(0, len(encoded), 5)])
 
 def decode(cipher_text):
-    normalized = [c for c in cipher_text 
-                    if c.isalnum()]
+    normalized = [c for c in cipher_text if c.isalnum()]
     return translation(''.join(normalized).lower())
