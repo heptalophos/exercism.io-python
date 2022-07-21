d4c77d619f76c0fc42f5c1a25685992957d0b0d3
Zero Delta <zerodeltahedge@gmail.com>
2022-07-21 19:50:45 +0300

    Refactor collatz-conjecture

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,14 +1,12 @@
 def steps(number):
     if number <= 0:
-        raise ValueError(
-                "Only positive integers are allowed"
-              )
+        raise ValueError("Only positive integers are allowed")
     _steps = 0
     while number != 1:
         if number % 2:
             number *= 3
             number += 1
         else:
-            number //= 2
+            number >>= 1
         _steps += 1
     return _steps
