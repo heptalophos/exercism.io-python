5dce86faac17e9f3da5ac77e2b45566e8ffd39f5
Zero Delta <zerodeltahedge@gmail.com>
2022-09-06 13:21:07 +0300

    Update all_your_base.py

--- all-your-base/all_your_base.py
+++ all-your-base/all_your_base.py
@@ -1,2 +1,13 @@
+invalid_base = lambda b: b < 2
+valid_digits = lambda ds, b: all(0 <= d < b for d in ds)
+errors = [ "input base must be >= 2", 
+           "output base must be >= 2", 
+           "all digits must satisfy 0 <= d < input base" ]  
+
 def rebase(input_base, digits, output_base):
-    pass
+    if invalid_base(input_base):
+        raise ValueError(errors[0])
+    if invalid_base(output_base)
+        raise ValueError(errors[1])
+    if not valid_digits(digits, input_base):
+        raise ValueError(errors[2])
