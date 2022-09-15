c9242aab6c1d2fae973843d3cf26751db99bfdf9
Zero Delta <zerodeltahedge@gmail.com>
2022-09-15 20:15:26 +0300

    Update resistor_color_duo.py

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,13 @@
-def value(colors):
-    pass
+from functools import reduce
+
+colors = ['black', 'brown', 'red', 'orange', 'yellow',
+          'green', 'blue', 'violet', 'grey', 'white' ]
+
+color_code = lambda color: colors.index(color)
+
+def value(colors: 'list[str]'):
+    return reduce (
+        lambda band1, band2: 10 * band1 + band2,
+        map(color_code, colors[:2]) 
+    )
+    
