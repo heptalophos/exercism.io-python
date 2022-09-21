1dcc62b646e24359cdf6a77d2583cee9c9d0f909
Zero Delta <zerodeltahedge@gmail.com>
2022-09-21 23:13:21 +0300

    Reformat anagram.py

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -4,6 +4,7 @@ def find_anagrams(word, candidates):
     are anagrams of word
     """
     normalized = lambda x: sorted(x.lower())
-    anagrams = [cand for cand in candidates if normalized(cand) == normalized(word)
-                                               and cand.lower() != word.lower()]
+    anagrams = [cand for cand in candidates 
+                     if normalized(cand) == normalized(word)
+                        and cand.lower() != word.lower()]
     return anagrams
