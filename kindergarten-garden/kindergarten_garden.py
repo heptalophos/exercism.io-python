3359ffcbdd0c33b3bf835b753c2001cbe5956add
Zero Delta <zerodeltahedge@gmail.com>
2022-10-13 14:30:27 +0300

    Update kindergarten_garden.py

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,19 +1,10 @@
 class Garden(object):
     
-    CHILDREN = ['Alice', 'Bob', 
-                'Charlie', 'David', 
-                'Eve', 'Fred', 
-                'Ginny', 'Harriet', 
-                'Ileana', 'Joseph', 
-                'Kincaid', 'Larry', 
-                'Samantha', 'Patricia', 
-                'Xander', 'Roger']
+    CHILDREN = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny', 
+                'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry', 'Samantha', 
+                'Patricia', 'Xander', 'Roger']
     
-    PLANTS = {p[0]: p 
-              for p in ['Clover', 
-                        'Grass', 
-                        'Radishes', 
-                        'Violets']}
+    PLANTS = {p[0]: p for p in ['Clover', 'Grass', 'Radishes', 'Violets']}
 
     def __init__(self, diagram, students=None):
         self.rows = diagram.split('\n')
@@ -22,8 +13,5 @@ class Garden(object):
 
     def plants(self, child):
         i = self.students.index(child)
-        plants = [[self.PLANTS[plant] 
-                   for plant in row] 
-                   for row in self.rows]
-        return sum([row[0 + 2 * i:2 + 2 * i] 
-                   for row in plants], [])
+        plants = [[self.PLANTS[plant] for plant in row] for row in self.rows]
+        return sum([row[0 + 2 * i:2 + 2 * i] for row in plants], [])
