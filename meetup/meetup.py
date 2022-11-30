0ee79fd347891d4916fe3b63de051b6988282ef9
Zero Delta <zerodeltahedge@gmail.com>
2022-12-01 01:27:25 +0200

    Refactor meetup

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -6,19 +6,19 @@ DAYS = { 'Monday': 0,
          'Thursday': 3, 
          'Friday' : 4, 
          'Saturday': 5, 
-         'Sunday': 6 }
+         'Sunday': 6}
 
 last = lambda d : d.month != (d + timedelta(days=7)).month
 teenth = lambda d : d.day > 12 and d.day < 20
 nth = lambda n : lambda d : (d.day - 1) // 7 == n - 1 
 
-NTH = { 'teenth': teenth, 
-        'last': last, 
-        '1st': nth(1), 
-        '2nd': nth(2), 
-        '3rd': nth(3), 
-        '4th': nth(4), 
-        '5th': nth(5) }
+NTH = {'teenth': teenth, 
+       'last': last, 
+       'first': nth(1), 
+       'second': nth(2), 
+       'third': nth(3), 
+       'fourth': nth(4), 
+       'fifth': nth(5)}
 
 def meetup(year, month, which, weekday):
     dt = date(year, month, 1)
@@ -26,8 +26,7 @@ def meetup(year, month, which, weekday):
         if dt.weekday() == DAYS[weekday] and NTH[which](dt):
             return dt
         dt += timedelta(days=1)
-    raise MeetupDayException("Not a valid meetup day")
-
+    raise MeetupDayException("That day does not exist.")
 
 class MeetupDayException(ValueError):
     pass
