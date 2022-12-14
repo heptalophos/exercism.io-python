fbdf8fddc391cc7f5314ae3d87827b93af8bd28d
Zero Delta <zerodeltahedge@gmail.com>
2022-12-14 23:45:07 +0200

    Update pig_latin.py

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,15 +1,2 @@
-def pig_latin(word):
-    if word.startswith (('a', 'e', 'i', 'o', 'u')):
-        return word + 'ay'
-    if word.startswith (('thr', 'sch', 'squ')):
-        return word[3:] + word[:3] + 'ay'
-    if word.startswith (('ch', 'qu', 'th', 'rh')):
-        return word[2:] + word[:2] + 'ay'
-    if word.startswith (('ye', 'xe')):
-        return word[1:] + word[:1] + 'ay'
-    if word.startswith (('yt', 'xr')):
-        return word + 'ay'
-    return word[1:] + word[:1] + 'ay'
-        
-def translate(sentence):
-    return ' '.join([pig_latin(w) for w in sentence.lower().split()])
+def translate(text):
+    pass
