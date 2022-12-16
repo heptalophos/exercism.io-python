31b2287b426ba8886a6a9989c88c5f8f64c64f18
Zero Delta <zerodeltahedge@gmail.com>
2022-12-16 23:26:59 +0200

    Update transpose.py

--- transpose/transpose.py
+++ transpose/transpose.py
@@ -1,2 +1,13 @@
 def transpose(lines):
-    pass
+    if not lines:
+        return ''
+    matrix = []
+    for row, line in enumerate(lines.splitlines()):
+        for col, ch in enumerate(line):
+            while len(matrix) <= col:
+                matrix.append([])
+            while len(matrix[col]) <= row:
+                matrix[col].append(' ')
+            matrix[col].append(ch)
+    m = [l.rstrip().replace('-', ' ') for l in matrix]
+    return '\n'.join(''.join(r) for r in m)
