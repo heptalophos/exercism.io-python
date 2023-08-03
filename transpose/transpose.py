11efe42276af8e86eb49557cda24d3da09e63666
Zero Delta <zerodeltahedge@gmail.com>
2023-08-03 19:43:24 +0300

    Refactor transpose

--- transpose/transpose.py
+++ transpose/transpose.py
@@ -1,2 +1,12 @@
 def transpose(lines):
-    pass
+    if not lines:
+        return ''
+    matrix = []
+    for row, line in enumerate(lines.splitlines()):
+        for col, ch in enumerate(line):
+            while len(matrix) <= col:
+                matrix.append([])
+            while len(matrix[col]) < row:
+                matrix[col].append(' ')
+            matrix[col].append(ch)
+    return '\n'.join(''.join(r) for r in matrix)
