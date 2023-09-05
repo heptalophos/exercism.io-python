4a59a960292af9d4c1ab43d12ecdd6cb3a67ed6a
Zero Delta <zerodeltahedge@gmail.com>
2023-09-05 23:04:19 +0300

    Update sublist.py

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -1,25 +1,21 @@
-from collections import deque
+SUPERLIST, EQUAL, SUBLIST, UNEQUAL = 3, 2, 1, 0
 
-SUBLIST, SUPERLIST, EQUAL, UNEQUAL = 3, 2, 1, 0
+def swapper(func):
+    """Wrapper that ensures the first argument is smaller then the seconds"""
+    def swap(first, second):
+        """swaps first and second argument if the second is smaller"""
+        if first == second:
+            return 2
+        if len(first) > len(second):
+            return SUPERLIST * func(second, first)
+        else:
+            return func(first, second)
+    return swap
 
-def sublist(l1, l2):
-    if l1 == [] and l2:
-        return SUBLIST
-    if l2 == [] and l1:
-        return SUPERLIST
-    if len(l1) == len(l2):
-        return EQUAL * int(l1 == l2)
-    if len(l1) < len(l2):
-        sub, sup = l1, l2 
-    else:
-        sub, sup = l2, l1
-    ls = deque([], len(sub))
-    try:
-        for x in sup:
-            ls.append(x)
-            if list(ls) == sub:
-                return SUBLIST if sub == l1 else SUPERLIST
-    except:
-        return EQUAL
-    finally:
-        return UNEQUAL
+@swapper
+def sublist(small_list, big_list):
+    """Checks if first list is a sublist of the seconds"""
+    for i in range(len(big_list) - len(small_list) + 1):
+        if small_list == big_list[i: i+len(small_list)]:
+            return SUBLIST
+    return UNEQUAL
