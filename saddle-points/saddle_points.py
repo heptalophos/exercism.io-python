6ac8b5aa5ccfe0db9355f69de791630c87d83132
Zero Delta <zerodeltahedge@gmail.com>
2023-09-06 22:46:24 +0300

    Update saddle_points.py

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -2,7 +2,7 @@ def saddle_points(matrix):
     if not matrix:
         return set()
     if any(len(row) != len(matrix[0]) for row in matrix):
-            raise ValueError("irregular matrix")
+        raise ValueError("irregular matrix")
     columns = list(zip(*matrix))
     return [{"row": rx, "column": cx} 
             for rx, row in enumerate(matrix, 1)
