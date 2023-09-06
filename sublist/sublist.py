7f39913adcd0352cc85c9dde666b226bb0a322d4
Zero Delta <zerodeltahedge@gmail.com>
2023-09-06 22:41:03 +0300

    Refactor sublist

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -1,21 +1,22 @@
 SUPERLIST, EQUAL, SUBLIST, UNEQUAL = 3, 2, 1, 0
 
 def swapper(func):
-    """Wrapper that ensures the first argument is smaller then the seconds"""
+    """Wrapper that ensures the first argument 
+       is smaller then the second"""
     def swap(first, second):
-        """swaps first and second argument if the second is smaller"""
+        """swaps first and second argument 
+           if the second is smaller"""
         if first == second:
-            return 2
+            return EQUAL
         if len(first) > len(second):
-            return 3 * func(second, first)
+            return SUPERLIST * func(second, first)
         else:
             return func(first, second)
     return swap
 
 @swapper
-def sublist(small_list, big_list):
-    """Checks if first list is a sublist of the seconds"""
-    for i in range(len(big_list) - len(small_list) + 1):
-        if small_list == big_list[i: i+len(small_list)]:
-            return 1
-    return 0
+def sublist(sub, sup):
+    for i in range(len(sup) - len(sub) + 1):
+        if sub == sup[i:i + len(sub)]:
+            return SUBLIST
+    return UNEQUAL
