40db1cec17ca92b0a9b9224f6dc5f6c6896dafad
Zero Delta <zerodeltahedge@gmail.com>
2023-09-26 16:16:34 +0300

    Update queen_attack.py

--- queen-attack/queen_attack.py
+++ queen-attack/queen_attack.py
@@ -1,6 +1,17 @@
 class Queen:
-    def __init__(self, row, column):
-        pass
+    def __init__(self, rank, file):
+        negative = "row" if rank < 0 else "column" if file < 0 else "ok"
+        if negative != "ok":
+            raise ValueError(f'{negative} not positive')
+        outsider = "row" if rank > 7 else "column" if file > 7 else "ok"
+        if outsider !="ok":
+            raise ValueError(f'{outsider} not on board')
+        self.rank, self.file = rank, file
 
     def can_attack(self, another_queen):
-        pass
+        err = 'Invalid queen position: both queens in the same square'
+        drank = abs(self.rank - another_queen.rank)
+        dfile = abs(self.file - another_queen.file)
+        if drank == dfile == 0:
+            raise ValueError(err)
+        return drank * dfile == 0 or drank / dfile == 1
