84f0fff9e85459d9b1745b6284cf2e0054f54ee0
Zero Delta <zerodeltahedge@gmail.com>
2023-10-09 20:00:25 +0300

    Reformat proverb

--- proverb/proverb.py
+++ proverb/proverb.py
@@ -1,16 +1,3 @@
-def proverb(*input_data, qualifier=None):
-    if not input_data: return []
-    prime_cause = input_data[0]
-    if qualifier: prime_cause = f'{qualifier} {prime_cause}'
-    verses, epilogue = [], f'And all for the want of a {prime_cause}.'
-    for pair in pairs(input_data):
-        (cause, result) = pair 
-        premise = f'For want of a {cause} '
-        conclusion = f'the {result} was lost.'
-        verses.append(f'{premise}{conclusion}')
-    verses.append(epilogue)
-    return verses
-
 def pairs(input_list):
     iterable = iter(input_list) 
     nextitem = next(iterable)
@@ -20,5 +7,17 @@ def pairs(input_list):
             yield (item, nextitem)
         except StopIteration:
             return     
-                      
 
+def proverb(*input_data, qualifier=None):
+    if not input_data: return []
+    prime_cause = input_data[0]
+    if qualifier: prime_cause = f'{qualifier} {prime_cause}'
+    epilogue = f'And all for the want of a {prime_cause}.'
+    verses = [] 
+    for pair in pairs(input_data):
+        (cause, result) = pair 
+        premise = f'For want of a {cause} '
+        conclusion = f'the {result} was lost.'
+        verses.append(f'{premise}{conclusion}')
+    verses.append(epilogue)
+    return verses
