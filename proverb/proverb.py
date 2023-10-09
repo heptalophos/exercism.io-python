51723dfa52c44cfc8cd01ae4cbc0ab2194535974
Zero Delta <zerodeltahedge@gmail.com>
2023-10-09 21:14:57 +0300

    Update proverb.py

--- proverb/proverb.py
+++ proverb/proverb.py
@@ -12,11 +12,10 @@ def proverb(*input_data, qualifier=None):
     if not input_data: return []
     prime_cause = input_data[0]
     if qualifier: prime_cause = f'{qualifier} {prime_cause}'
-    epilogue = f'And all for the want of a {prime_cause}.'
-    verses = [] 
+    verses, epilogue = [], f'And all for the want of a {prime_cause}.'
     for pair in pairs(input_data):
         (cause, result) = pair 
-        premise = f'For want of a {cause} '
+        premise = f'For want of a {cause} ' 
         conclusion = f'the {result} was lost.'
         verses.append(f'{premise}{conclusion}')
     verses.append(epilogue)
