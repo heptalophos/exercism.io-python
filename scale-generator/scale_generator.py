f76efeb4d6f57934da3965f73210c4a9cfbc326a
Zero Delta <zerodeltahedge@gmail.com>
2023-10-12 17:30:31 +0300

    Refactor scale_generator.py

--- scale-generator/scale_generator.py
+++ scale-generator/scale_generator.py
@@ -1,17 +1,17 @@
 class Scale:
-    S_FLATS = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab']
-    S_SHARP = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
-    T_FLATS = ['F', 'Bb', 'Eb', 'Ab', 'Db', 'Gb', 'd', 'g', 'c', 'f','bb','eb']
+    S_FLATS = 'A Bb B C Db D Eb E F Gb G Ab'.split()
+    S_SHARP = 'A A# B C C# D D# E F F# G G#'.split()
+    T_FLATS = 'F Bb Eb Ab Db Gb d g c f bb eb'.split()
 
-    def __init__(self, tonic):
+    def __init__(self, tonic: str):
         self.scale = self.S_FLATS if tonic in self.T_FLATS else self.S_SHARP
         self.tonic = tonic.capitalize()
 
-    def chromatic(self):
+    def chromatic(self) -> list[str]:
         scale, index = self.scale, self.scale.index(self.tonic)
         return scale[index:] + scale[:index]
 
-    def interval(self, intervals):
+    def interval(self, intervals: str) -> list[str]:
         chromatic, notes, index = self.chromatic(), [], 0
         steps = {'m': 1, 'M': 2, 'A': 3}
         for i in intervals:
