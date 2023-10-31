ac70bf8d82d6517b87362142a6520db2ce8e9853
Zero Delta <zerodeltahedge@gmail.com>
2023-11-01 01:14:19 +0200

    Refactor bob

--- bob/bob.py
+++ bob/bob.py
@@ -1,17 +1,17 @@
+def response(text):
+    smalltalk = sanitized(text)
+    if silent(smalltalk):
+        return "Fine. Be that way!"
+    if yelling(smalltalk):
+        return "Calm down, I know what I'm doing!"
+    if shouting(smalltalk):
+        return "Whoa, chill out!"
+    if question(smalltalk):
+        return "Sure."
+    return "Whatever."
+
 sanitized = lambda x : x.strip()
 silent    = lambda x : not x
 shouting  = lambda x : x.isupper() 
 question  = lambda x : x.endswith('?')
 yelling   = lambda x : shouting(x) and question(x)
-
-def response(text):
-    text = sanitized(text)
-    if silent(text):
-        return "Fine. Be that way!"
-    if yelling(text):
-        return "Calm down, I know what I'm doing!"
-    if shouting(text):
-        return "Whoa, chill out!"
-    if question(text):
-        return "Sure."
-    return "Whatever."
