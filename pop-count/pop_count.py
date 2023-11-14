103accf3f8806ecd56b1b96b0a1af93be210f4be
Zero Delta <zerodeltahedge@gmail.com>
2023-11-15 01:15:08 +0200

    pop-count, 1st iteration (passing all)

--- pop-count/pop_count.py
+++ pop-count/pop_count.py
@@ -1,2 +1,9 @@
 def egg_count(display_value):
-    pass
+    """
+    Count the '1' bits in the binary representation of the `display_value` 
+    (without using "that bit-count functionality" (or any bitwise operators)).
+    
+    """
+    if display_value == 0: 
+        return 0
+    return display_value % 2 + egg_count(display_value // 2)
