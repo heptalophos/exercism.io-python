2235f2eb8b38f851e79936bfa22b12ae4fa28023
Zero Delta <zerodeltahedge@gmail.com>
2024-02-13 17:15:54 +0200

    Solve crypto-square

--- crypto-square/crypto_square.py
+++ crypto-square/crypto_square.py
@@ -1,2 +1,19 @@
+from string import whitespace, punctuation
+from itertools import zip_longest
+from math import ceil, sqrt
+
 def cipher_text(plain_text):
-    pass
+    othercharacters = whitespace + punctuation
+    lower_plain, sanitized = plain_text.casefold().strip(), ''
+    chunks = lambda n, ls: zip_longest(*[iter(ls)] * n, 
+                                       fillvalue=' ')
+    transpose = lambda m: zip(*m)
+    for ch in lower_plain:
+        if ch in othercharacters:
+            continue
+        sanitized += ch
+    if not sanitized:
+        return ""
+    sq_side = ceil(sqrt(len(sanitized)))
+    rows = chunks(sq_side, sanitized)
+    return ' '.join(''.join(c) for c in transpose(rows))
