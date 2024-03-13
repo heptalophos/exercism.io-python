b230558571cefdc8dd5e0d4a7f40c52fe42c6b48
Zero Delta <zerodeltahedge@gmail.com>
2024-03-13 23:30:07 +0200

    Update variable_length_quantity.py

--- variable-length-quantity/variable_length_quantity.py
+++ variable-length-quantity/variable_length_quantity.py
@@ -1,6 +1,25 @@
-def encode(numbers):
-    pass
+def encode(numbers: list[int]) -> list[int]:
+    encoded = []
+    for n in reversed(numbers):
+        if n == 0:
+            encoded += [0]
+        msbyte = 0
+        while n > 0:
+            encoded += [msbyte ^ (n & 127)]
+            msbyte = 128
+            n >>= 7
+    return encoded[::-1]
 
 
-def decode(bytes_):
-    pass
+def decode(bytes_: list[int]) -> list[int]:
+    decoded, n = [], 0
+    for idx, byte in enumerate(bytes_):
+        n = (n << 7) + (byte & 127)
+        if byte & 128 == 0:
+            decoded += [n]
+            n = 0
+        elif idx == len(bytes_) - 1:
+            raise ValueError('incomplete sequence')
+        else:
+            continue
+    return decoded 
