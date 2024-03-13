3f4f7dc4caebeaf22662ec7e483d95663536d53b
Zero Delta <zerodeltahedge@gmail.com>
2024-03-14 00:41:00 +0200

    Refactor resistor_color_expert.py

--- resistor-color-expert/resistor_color_expert.py
+++ resistor-color-expert/resistor_color_expert.py
@@ -9,18 +9,18 @@ def resistor_label(colors: list[str]) -> str:
     if len(colors) == 1: 
         return f'{r_code(colors[0])} ohms'
     tolerance = t_code(colors[-1])
-    zeros = r_code(colors[-2])
-    band2 = r_code(colors[-4])
-    band3 = r_code(colors[-3])
+    zeros     = r_code(colors[-2])
+    band2     = r_code(colors[-4])
+    band3     = r_code(colors[-3])
     if len(colors) == 4: 
         band1 = 0
     else: 
         band1 = r_code(colors[-5])
-    value = (band1 * 100 + band2 * 10 + band3) * 10 ** zeros
+    resistance = (band1 * 100 + band2 * 10 + band3) * 10 ** zeros
     ohms, magn = 0, ''
-    if value   > 1e9: ohms, magn = value / 1e9, 'giga'
-    elif value > 1e6: ohms, magn = value / 1e6, 'mega'
-    elif value > 1e3: ohms, magn = value / 1e3, 'kilo'
-    elif value > 1e0: ohms = value
+    if resistance   > 1e9: ohms, magn = resistance / 1e9, 'giga'
+    elif resistance > 1e6: ohms, magn = resistance / 1e6, 'mega'
+    elif resistance > 1e3: ohms, magn = resistance / 1e3, 'kilo'
+    elif resistance > 1e0: ohms = resistance
     else: ohms = 0
     return f'{ohms:g} {magn}ohms ±{tolerance}%'
